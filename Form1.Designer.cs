34e0fc08d051e48a2157f744c5a67bf2dd39af38
Igor <Igor_pavlenko@inbox.ru>
2019-06-17 16:54:17 +0600

    # Было добавлен уровень звука

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -40,11 +41,21 @@
             this.label1.Size = new System.Drawing.Size(0, 39);
             this.label1.TabIndex = 0;
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(207, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Уровень звука";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(321, 262);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -58,6 +69,7 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -19,11 +19,14 @@ namespace SpeechRecognition
         }
 
         static Label l;
+        static Label l2;
 
         static void sre_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
         {
-              if (e.Result.Confidence > 0.85) l.Text = e.Result.Text;
-               {
+            l2.Text = e.Result.Confidence.ToString();
+            if (e.Result.Confidence > 0.33) 
+            {
+                l.Text = e.Result.Text;
                 // if (e.Result.Text == "выключить"&& e.Result.Confidence > 0.87)System.Diagnostics.Process.Start("cmd", "/c shutdown -s -f -t 00");
                 if (e.Result.Text == "компьютер" && e.Result.Confidence > 0.80)
                    {string myComputerPath = Environment.GetFolderPath(Environment.SpecialFolder.MyComputer);
@@ -31,7 +34,7 @@ namespace SpeechRecognition
                  if (e.Result.Text == "браузер" && e.Result.Confidence > 0.87)System.Diagnostics.Process.Start("http://www.google.com");
                  if (e.Result.Text == "ютуб" && e.Result.Confidence > 0.87) System.Diagnostics.Process.Start("https://www.youtube.com");
                  if (e.Result.Text == "радио" && e.Result.Confidence > 0.87) System.Diagnostics.Process.Start(@"D:\исходник\Radio1\bin\Debug\Radio.exe");
-                }
+             }
             
             
         }    
@@ -39,7 +42,7 @@ namespace SpeechRecognition
         private void Form1_Shown(object sender, EventArgs e)
         {
             l = label1;
-       
+            l2 = label2;
             System.Globalization.CultureInfo ci = new System.Globalization.CultureInfo("ru-ru");
             SpeechRecognitionEngine sre = new SpeechRecognitionEngine(ci);
             sre.SetInputToDefaultAudioDevice();
@@ -64,7 +67,7 @@ namespace SpeechRecognition
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            
         }
     }
 }
