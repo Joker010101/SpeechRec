7f1abef85566eb9c7286a945ff7b10e60b9e2bec
Igor <Igor_pavlenko@inbox.ru>
2021-02-04 12:04:36 +0600

    Добавил Cheken

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // label1
@@ -59,17 +60,28 @@
             this.label3.TabIndex = 1;
             this.label3.Text = "Уровень звука";
             // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(13, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(75, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Включить";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(321, 262);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-          
             this.Shown += new System.EventHandler(this.Form1_Shown);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -81,6 +93,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,13 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
+﻿using Microsoft.Speech.Recognition;
+using System;
 using System.Windows.Forms;
-using Microsoft.Speech.Recognition;
-using System.Diagnostics;
 
 namespace SpeechRecognition
 {
@@ -22,14 +15,20 @@ namespace SpeechRecognition
         static Label l2;
 
 
-       
 
-// proverka 2 
+
+        // proverka 2 
 
         static void sre_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
         {
+
+
+
+
+
+
             l2.Text = e.Result.Confidence.ToString();
-            if (e.Result.Confidence > 0.33) 
+            if (e.Result.Confidence > 0.33)
             {
                 l.Text = e.Result.Text;
                 // if (e.Result.Text == "выключить"&& e.Result.Confidence > 0.87)System.Diagnostics.Process.Start("cmd", "/c shutdown -s -f -t 00");
@@ -38,26 +37,32 @@ namespace SpeechRecognition
                     string myComputerPath = Environment.GetFolderPath(Environment.SpecialFolder.MyComputer);
                     System.Diagnostics.Process.Start("explorer", myComputerPath);
                 };
-                 if (e.Result.Text == "браузер" && e.Result.Confidence > 0.87)System.Diagnostics.Process.Start("http://www.google.com");
-                 if (e.Result.Text == "ютуб" && e.Result.Confidence > 0.87) System.Diagnostics.Process.Start("https://www.youtube.com");
+                if (e.Result.Text == "браузер" && e.Result.Confidence > 0.87) System.Diagnostics.Process.Start("http://www.google.com");
+                if (e.Result.Text == "ютуб" && e.Result.Confidence > 0.87) System.Diagnostics.Process.Start("https://www.youtube.com");
                 // if (e.Result.Text == "радио" && e.Result.Confidence > 0.87) System.Diagnostics.Process.Start(@"D:\исходник\Radio1\bin\Debug\Radio.exe");
                 if (e.Result.Text == "радио" && e.Result.Confidence > 0.87) System.Diagnostics.Process.Start(@"D:\исходники\radio\radio\radio\Radio\bin\Debug\Radio.exe");
-                if (e.Result.Text == "выход" && e.Result.Confidence > 0.87){MessageBox.Show("пока господин");Application.Exit();};
+                if (e.Result.Text == "выход" && e.Result.Confidence > 0.87) { MessageBox.Show("пока господин"); Application.Exit(); };
                 if (e.Result.Text == "пока" && e.Result.Confidence > 0.87) MessageBox.Show("надоела Вам");
-                
-                    
+
+
             }
-            
-            
-        }    
-        
+
+
+        }
+
+
+
+
+
+
         private void Form1_Shown(object sender, EventArgs e)
         {
-           
+
             l = label1;
             l2 = label2;
-            Speech();
-        
+
+
+
         }
 
 
@@ -81,11 +86,46 @@ namespace SpeechRecognition
 
         }
 
+        static void SpeechOff()
+        {
+
+
+            System.Globalization.CultureInfo ci = new System.Globalization.CultureInfo("ru-ru");
+            SpeechRecognitionEngine sre1 = new SpeechRecognitionEngine(ci);
+            sre1.SetInputToDefaultAudioDevice();
+            sre1.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(sre_SpeechRecognized);
+            sre1.RecognizeAsyncStop();
+            sre1.RecognizeAsyncCancel();
+
+
+
+        }
 
 
 
 
 
 
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+            CheckBox checkBox = (CheckBox)sender;
+
+            if (checkBox.Checked == true)
+            {
+                checkBox1.Text = "Выключить";
+                Speech();
+            }
+            else
+
+            {
+                checkBox1.Text = "Включить";
+                SpeechOff();
+                MessageBox.Show("dfd");
+            }
+
+
+        }
     }
 }
